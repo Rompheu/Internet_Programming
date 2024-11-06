37c2ece2a7ac49f81fb2a7fef158e103476ac499
Rompheu <rompher2609@gmail.com>
2024-11-06 22:38:45 +0700

    Step 4: done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,159 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  This is my first Vue.Js project
+  <div class="Wrapper">
+    <div class="Container" v-for="category in Categories">
+      <CategoryComponent  :key="category" 
+      :color="category.color"  
+      :name="category.name"
+      :quantity="category.quantity"
+      :picture="category.picture"
+      />
+    </div>
 
-  <RouterView />
+    <!-- <div class="PromotionContainer" v-for="promotion in Promotions">
+      <PromotionComponent  :key="promotion" 
+      :color="promotion.color"  
+      :text="promotion.text"
+      :picture="promotion.picture"
+      />
+    </div> -->
+  </div>
+  <div class="WrapperII">
+    <div class="PromotionContainer" v-for="promotion in Promotions">
+      <PromotionComponent  :key="promotion" 
+      :color="promotion.color"  
+      :text="promotion.text"
+      :picture="promotion.picture"
+      />
+    </div>
+  </div>
+
+<RouterView/>
 </template>
 
-<style scoped>
+<script>
+  import CategoryComponent from './components/ProductCategory.vue';
+  import PromotionComponent from './components/PromotionProduct.vue';
+  import { RouterView } from 'vue-router';
 
+  export default{
+    components: {
+        CategoryComponent,
+        PromotionComponent
+    },
+
+    data() {
+
+    return {
+      Categories:[
+        {
+          color:"#F2FCE4",
+          name:"Hamburger",
+          quantity:"14 items",    
+          picture: (new URL("@/assets/Image/cake & milk.png", import.meta.url))
+        },
+        {
+          color:"#FFFCEB" ,
+          name:"Peach",
+          quantity:"17 items",    
+          picture:(new URL("@/assets/Image/peach.png", import.meta.url))
+        },
+        {
+          color:"#ECFFEC" ,
+          name:"Oganic Kiwi",
+          quantity:"21 items",    
+          picture:(new URL("@/assets/Image/organic kiwi.png", import.meta.url))
+        },
+        {
+          color:"#FEEFEA" ,
+          name:"Red Apple",
+          quantity:"68 items",    
+          picture:(new URL("@/assets/Image/apple.png", import.meta.url))
+        },
+        {
+          color:"#FFF3EB" ,
+          name:"Snack",
+          quantity:"34 items",    
+          picture:(new URL("@/assets/Image/snack.png", import.meta.url))
+        },
+        {
+          color:"#FFF3FF" ,
+          name:"Black plum",
+          quantity:"25 items",    
+          picture:(new URL("@/assets/Image/plum.png", import.meta.url))
+        },
+        {
+          color:"#F2FCE4" ,
+          name:"Vegetable",
+          quantity:"65 items",    
+          picture:(new URL("@/assets/Image/vegetable.png", import.meta.url))
+        },
+        {
+          color:"#FFFCEB" ,
+          name:"Headphone",
+          quantity:"33 items",    
+          picture:(new URL("@/assets/Image/headphone.png", import.meta.url))
+        },
+
+        {
+          color:"#F2FCE4" ,
+          name:"Chocolate",
+          quantity:"54 items",    
+          picture:(new URL("@/assets/Image/snack1.png", import.meta.url))
+        },
+        {
+          color:"#FFF3FF" ,
+          name:"Orange",
+          quantity:"63 items",    
+          picture:(new URL("@/assets/Image/orange.png", import.meta.url))
+        }
+      ],
+
+      Promotions:[
+        {
+          color:"#F0E8D5",
+          text:"Everyday Fresh & Clean with Our Products",
+          picture:(new URL("@/assets/Image/Onion.png", import.meta.url))
+        },
+        {
+          color:"#F3E8E8",
+          text:"Make your Breakfast Healthy and Easy",
+          picture:(new URL("@/assets/Image/Juice.png", import.meta.url))
+        },
+        {
+          color:"#E7EAF3",
+          text:"The best Organic Product Online",
+          picture:(new URL("@/assets/Image/Veggie.png", import.meta.url))
+        },
+      ],
+    };
+  },
+  };
+
+</script>
+
+<style scoped>
+.Wrapper{  
+  display: flex;
+
+  width: 100vw;
+  overflow: hidden;
+  padding: 0px 20px;
+}
+
+.WrapperII {
+  display: flex;
+}
+
+.Container{
+  width: 80%;
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 10px;
+}
+
+.PromotionContainer{
+  width: 80%;
+  display: flex;
+  justify-content: space-evenly;
+}
 </style>
---
+++ src/components/BannerProduct.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="Container">
+        <span class="text">Shop Now</span> 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+  </svg>
+  
+    </div>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style scoped>
+  .size-6 {
+    width: 10px;
+  }
+    .Container{
+        display: flex;
+        align-items: center;
+        height: 20px;
+        width: 70px;
+        color: white;
+        font-size: 10px;
+        font-weight: bold;
+        background-color: #0cc770;
+        border-radius: 5px;
+        padding:3px 0px;
+    }
+  
+    .text{
+        display: flex;
+        align-items: center;
+        padding:5px;
+    }
+    .icon{
+        width: 10px;
+        color: white;
+    }
+  </style>
