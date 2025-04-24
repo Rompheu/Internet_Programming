ca7e88406636daf3797e397796a0db302815a347
Rompheu <rompher2609@gmail.com>
2025-04-24 18:29:55 -0400

    Finish all task of TP8

--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -32,6 +32,8 @@ class ProductController extends Controller
     public function updateProduct(Request $request, $productId) {
         $product = Product::find($productId);
         $product->name = $request->name;
+        $product->category_id = $request->category_id;
+        $product->pricing = $request->pricing;
         $product->save();
         return $product;
     }
